da9f17671093a4e44356a504b675b4bc64db0247
Corneliousm808 <corneliousmooring@gmail.com>
2016-11-21 08:41:54 -1000

    first push

---
+++ calculator.js
@@ -0,0 +1,23 @@
+// arrays
+
+var tan = ["john", "jon", "cool", 3, 11, 7, 9, null, undefined, ["no shirt" 27, null]
+console.log(tan[9][0]);
+
+// objects
+var yoloSwag = {
+  isaah : "beast",
+  hair : "sunshine",
+  sayings : ["wait, what?", "hold me im cold", "yoloha"]
+  position : "kicker",
+};
+console.log(yoloSwag);
+
+var yoloSwag = {
+  isaah : "beast",
+  hair : "sunshine",
+  sayings : ["wait, what?", "hold me im cold", "yoloha"]
+  position : "kicker",
+};
+console.log(yoloSwag.sayings[2]);
+// or
+console.log(yoloSwag["sayings"][1])
